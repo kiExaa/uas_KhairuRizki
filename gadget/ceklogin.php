fc4561f5ba6f64f4e911fb4e8a0ff12d95dff61e
kiExaa <khairulrizki2005@gmail.com>
2026-02-27 17:51:42 +0700

    Update ceklogin.php

--- gadget/ceklogin.php
+++ gadget/ceklogin.php
@@ -1,6 +1,6 @@
 <?php 
 session_start();
 if(!isset($_SESSION['ses_email']) AND !isset($_COOKIE['coo_email'])){
-    header("http://localhost/uas_KhairulRizki/gadget/");
+    header("http://localhost/uas_KhairuRizki/gadget/");
 }
 ?>
