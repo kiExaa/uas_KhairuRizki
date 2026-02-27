58e33d69879424566f734583528e357ab6154615
kiExaa <khairulrizki2005@gmail.com>
2026-02-27 20:30:40 +0700

    Create index.php

---
+++ gadget/logout/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Logout</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body style="background-color:#d1e6d4">
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-6">
+                <div class="card shadow">
+                    <div class="card-header">
+                        <b>Logout</b>
+                    </div>
+                    <div class="card-body text-center">
+                        <form action="logout.php" method="post">
+                            <a href="logout.php" class="btn btn-danger">Logout</a>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
+
+
