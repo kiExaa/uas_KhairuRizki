3ebe592a6242576307375bb196d8eed61cf79ce9
kiExaa <khairulrizki2005@gmail.com>
2026-02-27 16:49:01 +0700

    Create formedit.php

---
+++ gadget/kategori/formedit.php
@@ -0,0 +1,65 @@
+<?php
+include_once("../koneksi.php");
+$idedit = $_GET['id'];
+$qry = "SELECT * FROM kategori WHERE id='$idedit'";
+$edit = mysqli_query($koneksi,$qry);
+$data = mysqli_fetch_array($edit);
+?>
+<!DOCTYPE html>
+<html lang="en">.
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jurusan</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+
+<body style="background-color:#d1e6d4">
+    <?php
+    include_once("../navbar.php");
+    ?>
+
+    <div class="container">
+        <div class="row my-5">
+            <div class="col-8 m-auto">
+                <div class="card shadow p-3 mb-5 bg-body-tertiary rounded">
+                    <div class="card-header">
+                        <b>EDIT merk</b>
+                    </div>
+                    <div class="card-body">
+                        <form action="proses_edit.php" method="POST" enctype="multipart/form-data">
+                            <input type="hidden" name="id" value="<?=$data['id']?>">
+                            <div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">Kategori</label>
+                                <select class="form-control" name="nama_kategori" id="">
+                                    <option value="">-Pilih Kategori-</option>
+                                    <option value="IK">Smartphone</option>
+                                    <option value="SK">Laptop</option>
+                                </select>
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">Kode</label>
+                                <select class="form-control" name="kode_k" id="">
+                                    <option value="">-Pilih Kode-</option>
+                                    <option value="K01">K01</option>
+                                    <option value="K02">K02</option>
+                                </select>
+                            </div>
+                            
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
